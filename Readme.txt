18014cb03d8ba2cd51142057513ead3bb8358246
Mgsheikh <md2005ghufran@gmail.com>
2022-12-24 01:19:39 +0530

    initial commit

---
+++ Readme.txt
@@ -0,0 +1,11 @@
+Author: Md. Ghufran Sheikh
+Reason: For PHP Development Intership Assignments of Geany Softech Private Limited
+Author Email: md2005ghufran@gmail.com 
+
+
+#####
+
+Instructions -
+
+1. Import the attached users.sql in your database.
+2. Update config.php.
---
+++ index.php
@@ -0,0 +1,157 @@
+<?php 
+include "config.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Create Registration form with MySQL and PHP using Bootstrap</title>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<!-- Bootstrap JS -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	
+
+	<?php 
+	$error_message = "";$success_message = "";
+
+	// Register user
+	if(isset($_POST['btnsignup'])){
+		$fname = trim($_POST['fname']);
+		$lname = trim($_POST['lname']);
+		$email = trim($_POST['email']);
+		$password = trim($_POST['password']);
+		$confirmpassword = trim($_POST['confirmpassword']);
+
+		$isValid = true;
+
+		// Check fields are empty or not
+		if($fname == '' || $lname == '' || $email == '' || $password == '' || $confirmpassword == ''){
+			$isValid = false;
+			$error_message = "Please fill all fields.";
+		}
+
+		// Check if confirm password matching or not
+		if($isValid && ($password != $confirmpassword) ){
+			$isValid = false;
+			$error_message = "Confirm password not matching.";
+		}
+
+		// Check if Email-ID is valid or not
+		if ($isValid && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		  	$isValid = false;
+		  	$error_message = "Invalid Email-ID.";
+		}
+
+		if($isValid){
+
+			// Check if Email-ID already exists
+			$stmt = $con->prepare("SELECT * FROM users WHERE email = ?");
+			$stmt->bind_param("s", $email);
+			$stmt->execute();
+			$result = $stmt->get_result();
+			$stmt->close();
+			if($result->num_rows > 0){
+				$isValid = false;
+				$error_message = "Email-ID is already existed.";
+			}
+			
+		}
+
+		// Insert records
+		if($isValid){
+			$insertSQL = "INSERT INTO users(fname,lname,email,password ) values(?,?,?,?)";
+			$stmt = $con->prepare($insertSQL);
+			$stmt->bind_param("ssss",$fname,$lname,$email,$password);
+			$stmt->execute();
+			$stmt->close();
+
+			$success_message = "Account created successfully.";
+		}
+	}
+	?>
+</head>
+<body>
+	<div class='container'>
+		<div class='row'>
+			<div class='col-md-12'>
+				<h2></h2>
+			</div>
+
+			<div class='col-md-6' >
+					
+				<form method='post' action=''>
+
+					<h1>Please, Register</h1>
+					<?php 
+					// Display Error message
+					if(!empty($error_message)){
+					?>
+						<div class="alert alert-danger">
+						  	<strong>Error!</strong> <?= $error_message ?>
+						</div>
+
+					<?php
+					}
+					?>
+
+					<?php 
+					// Display Success message
+					if(!empty($success_message)){
+					?>
+						<div class="alert alert-success">
+						  	<strong>Success!</strong> <?= $success_message ?>
+						</div>
+
+					<?php
+					}
+					?>
+				
+					<div class="form-group">
+					    <label for="fname">First Name:</label>
+					    <input type="text" class="form-control" name="fname" id="fname" required="required" maxlength="80">
+					</div>
+					<div class="form-group">
+					    <label for="lname">Last Name:</label>
+					    <input type="text" class="form-control" name="lname" id="lname" required="required" maxlength="80">
+					</div>
+					<div class="form-group">
+					<label for="gender">Select your Gender
+                       <select name="referrer">
+                       <option value="">(select one)</option>
+                       <option value="1">Male</option>
+                       <option value="2">Female</option>
+                       <option value="3">Transgender</option>
+                       <option value="4">Other</option>
+                     </select>
+		                </label>
+				</div>      
+			              <div class="form-group">
+					    <label for="email">Email address:</label>
+					    <input type="email" class="form-control" name="email" id="email" required="required" maxlength="80">
+					</div>
+					<div class="form-group">
+					   <label class="pwd">Create a New Password:</label>
+					 <input type="password" name="password" pattern="[a-z0-5]{8,}" required>
+					 </div>
+					<div class="form-group">
+					    <label for="pwd">Confirm Password:</label>
+					    <input type="password" class="form-control" name="confirmpassword" id="confirmpassword" required="required" maxlength="80">
+					</div>
+					<div class="form-group">
+					<label for="mobile">Mobile Number: <input type="mobile" name="mobile" required /></label>
+					<br>
+					<br>
+					<button type="submit" name="btnsignup" class="btn btn-default">Submit</button>
+				</form>
+			</div>
+			
+			
+		</div>
+	</div>
+</body>
+</html>
